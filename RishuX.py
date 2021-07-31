782f20454f5f4143476789faefca41ccaef61ea9
RishuKhan <87880911+RishuKhan@users.noreply.github.com>
2021-07-31 11:27:53 +0530

    Update RishuX.py

--- RishuX.py
+++ RishuX.py
@@ -270,7 +270,7 @@ def logo():
                                              \033[1;91mhttps://www.facebook.com/Rishu.X.420
 ╚══════════════════════════════•ೋೋ•═════════════════════════════╝"""   
                                          
-def _cek_():
+def_cek_():
     global token
     try:
         token = open('login.txt', 'r').read()
